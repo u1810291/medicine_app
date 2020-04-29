4ea0decf50284882cda4f768e9668257c4531be4
u1810291 <otash158@bk.ru>
2020-04-29 21:45:10 +0500

    Initial commit from android studio version control

---
+++ app/src/main/java/com/example/medapp/ui/Register.java
@@ -0,0 +1,16 @@
+package com.example.medapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.medapp.R;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
